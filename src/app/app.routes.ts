bd9dc8541713503ac31891e44229882716b4bc76
shivam sahu <41139996+shiv112@users.noreply.github.com>
2025-04-09 16:28:07 +0530

    application setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login', // 👈 Your desired default route
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+];
